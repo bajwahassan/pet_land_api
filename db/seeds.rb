45507348a7bdc1bc8387ca7c96704dfd0fdc41bc
hbajwaa <bajwahassan3@outlook.com>
2020-08-15 20:11:16 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.create(name: "tom", age: 0, species: "cat", breed: "siamese", description: "playful kitten")
+Pet.create(name: "bozo", age: 1, species: "dog", breed: "gsd", description: "friendly puppy")
+Pet.create(name: "tim", age: 0, species: "parrot", breed: "african", description: "knows how to sing")
+Pet.create(name: "diego", age: 5, species: "dog", breed: "gsd", description: "guard dog")
